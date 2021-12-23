aecc52070654735c4a57e6123aa40f6686af7bef
testertv <61566646+testertv@users.noreply.github.com>
2021-12-23 13:47:10 +0100

    Update player.js

--- player/player.js
+++ player/player.js
@@ -9,7 +9,7 @@ function playM3u8(url){
       hls.attachMedia(video);
       hls.on(Hls.Events.MANIFEST_PARSED,function() {
         video.play();
-	video.controls = false;    
+	video.controls = false;   // disable control panel  
       });
       document.title = url
     }
