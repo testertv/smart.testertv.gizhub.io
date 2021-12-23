cba7014eca11162386bf653b4e56608aefffa0bf
testertv <61566646+testertv@users.noreply.github.com>
2021-12-23 16:51:05 +0100

    Update player.js

--- player/player.js
+++ player/player.js
@@ -9,13 +9,15 @@ function playM3u8(url){
       hls.attachMedia(video);
       hls.on(Hls.Events.MANIFEST_PARSED,function() {
         video.play();
+	video.controls = true;   // disable control panel
       });
       document.title = url
     }
 	else if (video.canPlayType('application/vnd.apple.mpegurl')) {
 		video.src = url;
 		video.addEventListener('canplay',function() {
-		  video.play();
+		video.play();
+		video.controls = true;   // disable control panel
 		});
 		video.volume = 0.3;
 		document.title = url;
