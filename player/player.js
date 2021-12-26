6d9c5484c4b33399e2c59b710ffd3af913c7bf72
testertv <61566646+testertv@users.noreply.github.com>
2021-12-26 01:20:47 +0100

    Update player.js

--- player/player.js
+++ player/player.js
@@ -42,7 +42,7 @@ function volumeDown() {
 //}
 
 function seekRight() {
-    <script type="text/javascript" src="../next_channels.js"></script>
+    <script type="text/javascript" src="../"></script>
 }
 
 //function seekLeft() {
@@ -50,7 +50,7 @@ function seekRight() {
 //}
 
 function seekLeft() {    
-    <script type="text/javascript" src="../previously_channels.js"></script>
+    <script type="text/javascript" src="../"></script>
 }	
 	
 function menu() {
